7029f28086d286fbfb93e1d0baaced602f498c04
gooLITE <goolitesum@gmail.com>
2021-10-24 00:35:47 +0800

    Initial Commit

---
+++ Expenses&Income/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Expenses&Income
+//
+//  Created by Yu Sum Yung on 24/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
